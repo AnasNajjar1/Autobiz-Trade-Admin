6b49825e33e5bbbbf0e10964a47c4b7a8db29d21
s.wangchuk <s.wangchuk@autobiz.com>
2020-01-14 14:53:52 +0100

    ajouter custom damages

---
+++ src/assets/choices/zone.js
@@ -0,0 +1,8 @@
+export default [
+    {id: "servicing", name: "servicing"},
+    {id: "wheels", name: "wheels"},
+    {id: "body", name:  "body"},
+    {id: "inner", name: "inner"},
+    {id: "road_test",name: "road_test"},
+    {id: "motor", name: "motor"}
+]
