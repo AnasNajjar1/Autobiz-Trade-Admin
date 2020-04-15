20a358f48f215ccf327590a96653e09203875053
b.chandon <b.chandon@autobiz.com>
2020-04-15 16:47:31 +0200

    edit api urls

--- src/config.js
+++ src/config.js
@@ -2,14 +2,14 @@ const ENV = process.env.REACT_APP_ENV;
 
 const apisB2bPlateform = {
   dev: "http://localhost:4000",
-  staging: "https://eob0vo3eq3.execute-api.eu-west-1.amazonaws.com/dev",
-  prod : "https://ttqmvnrudk.execute-api.eu-west-1.amazonaws.com/prod"
+  staging: "https://stg72-api-trade.shakazoola.com",
+  prod: "https://api-trade.autobiz.com"
 };
 exports.B2B_API = apisB2bPlateform[ENV];
 
 const autobizTradeUlrs = {
   dev: "https://stg72-trade.shakazoola.com",
   staging: "https://stg72-trade.shakazoola.com",
-  prod : "https://trade.autobiz.com"
+  prod: "https://trade.autobiz.com"
 };
 exports.TRADE_URL = autobizTradeUlrs[ENV];
