86ec40be4d9d7b696286315fa6f4123fc210fce9
Marceau <3538880-marceau.a@users.noreply.gitlab.com>
2020-11-19 11:27:37 +0100

    set winner

---
+++ src/vehicle/SetWinnerButton.js
@@ -0,0 +1,66 @@
+import React, { Fragment, useState } from "react";
+import {
+  Button,
+  Confirm,
+  useRefresh,
+  useNotify,
+  useTranslate,
+} from "react-admin";
+import Done from "@material-ui/icons/Done";
+import { API } from "aws-amplify";
+
+const SetWinnerButton = (props) => {
+  const translate = useTranslate();
+  const [open, setOpen] = useState(false);
+  const refresh = useRefresh();
+  const notify = useNotify();
+
+  const handleUpdate = async () => {
+    const { record } = props;
+
+    try {
+      await API.post("b2bPlateform", `/admin/sale/winner`, {
+        body: {
+          winner: record.userId,
+          saleId: record.saleId,
+        },
+      });
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  const handleClick = () => setOpen(true);
+  const handleDialogClose = () => setOpen(false);
+
+  const handleConfirm = async () => {
+    await handleUpdate();
+    refresh();
+    notify(translate("accept_submission"), "warning");
+
+    setOpen(false);
+  };
+
+  console.log(props.record);
+
+  if (props.record && props.record.requestWinner) {
+    return (
+      <Fragment>
+        <Button label={translate("accept_submission")} onClick={handleClick}>
+          <Done />
+        </Button>
+        <Confirm
+          isOpen={open}
+          title=""
+          content={translate("validation_submission_buyer")}
+          onConfirm={handleConfirm}
+          onClose={handleDialogClose}
+        />
+      </Fragment>
+    );
+  } else {
+    return <></>;
+  }
+};
+
+export default SetWinnerButton;
