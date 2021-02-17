a283fc9575dbcd50fa23d2efb84fc8a1aa964dd0
Bruno CHANDON <b.chandon@autobiz.com>
2021-02-17 09:57:55 +0100

    add ppr config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ const ENV = process.env.REACT_APP_ENV;
 
 const apisB2bPlateform = {
   dev: "http://localhost:4000",
-  staging: "https://e0ufgxwv4c.execute-api.eu-west-1.amazonaws.com/staging",
+  staging: "https://stg72-api-trade.shakazoola.com",
+  ppr: "https://ppr-api-trade.autobiz.com",
   prod: "https://api-trade.autobiz.com",
 };
 exports.B2B_API = apisB2bPlateform[ENV];
@@ -10,15 +11,16 @@ exports.B2B_API = apisB2bPlateform[ENV];
 const autobizTradeUlrs = {
   dev: "http://localhost:3000",
   staging: "https://stg72-trade.shakazoola.com",
+  ppr: "https://ppr-trade.autobiz.com",
   prod: "https://trade.autobiz.com",
 };
 exports.TRADE_URL = autobizTradeUlrs[ENV];
 
 const translationBucket = {
-  dev: "https://translations-host-dev.s3-eu-west-1.amazonaws.com/trade-admin",
-  staging:
-    "https://translations-host-dev.s3-eu-west-1.amazonaws.com/trade-admin",
-  prod: "https://translations-host-prod.s3-eu-west-1.amazonaws.com/trade-admin",
+  dev: "https://translations-host-dev.s3-eu-west-1.amazonaws.com/trade-app",
+  staging: "https://translations-host-dev.s3-eu-west-1.amazonaws.com/trade-app",
+  ppr: "https://translations-host-prod.s3-eu-west-1.amazonaws.com/trade-app",
+  prod: "https://translations-host-prod.s3-eu-west-1.amazonaws.com/trade-app",
 };
 
 exports.TRANSLATION_BUCKET = translationBucket[ENV];
