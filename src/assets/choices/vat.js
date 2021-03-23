5af9f3595f5b50207dcd184e45a70489dc5afbd5
Noura Nouioui <n.nouioui@autobiz.com>
2021-03-23 11:14:10 +0100

    add vat details field

---
+++ src/assets/choices/vat.js
@@ -0,0 +1,5 @@
+export default [
+  { id: true, name: "yes" },
+  { id: false, name: "no" },
+  { id: "", name: "dnk" },
+];
---
+++ src/assets/choices/vatDetails.js
@@ -0,0 +1,5 @@
+export default [
+  { id: "", name: "-" },
+  { id: "yes_50percent", name: "yes_50percent" },
+  { id: "yes_100percent", name: "yes_100percent" },
+];
