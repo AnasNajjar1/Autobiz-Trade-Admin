66d32bd4f334b1e1874d939ffadadde51755b2de
Sabrine Ben Sassi <s.bensassi@autobiz.com>
2021-08-02 15:34:15 +0100

    reassemble color of vehicle

---
+++ src/assets/choices/vehicleColor.js
@@ -0,0 +1,18 @@
+export const colorChoices = [
+  { id: "black", name: "black" },
+  { id: "white", name: "white" },
+  { id: "grey", name: "grey" },
+  { id: "blue", name: "blue" },
+  { id: "beige", name: "beige" },
+  { id: "red", name: "red" },
+  { id: "orange", name: "orange" },
+  { id: "brown", name: "brown" },
+  { id: "pink", name: "pink" },
+  { id: "green", name: "green" },
+  { id: "yellow", name: "yellow" },
+  { id: "purple ", name: "purple " },
+  { id: "sable ", name: "sable " },
+  { id: "moka ", name: "moka " },
+  { id: "anthracite", name: "anthracite" },
+  { id: "other", name: "other" },
+];
